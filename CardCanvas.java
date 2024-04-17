c513cd9299dd15cd2642a190f430f9537c17fce4
Julian Pasos <jpasos@uabc.edu.mx>
2024-04-17 08:47:15 -0700

    Se cambian imagenes de suits, Se hacen cambios al dibujar carta

--- CardCanvas.java
+++ CardCanvas.java
@@ -1,11 +1,14 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.swing.*;
+import javax.imageio.ImageIO;
 
 public class CardCanvas extends JPanel {
     private ArrayList<Card> cards;
+    private BufferedImage[] cardImages;
 
     public CardCanvas() {
         cards = new ArrayList<>();
@@ -28,20 +31,50 @@ public class CardCanvas extends JPanel {
         repaint(); // Vuelve a dibujar el lienzo con la nueva carta
     }
 
+    private int getSuitIndex(String suit) {
+        switch (suit) {
+            case "oros":
+                return 0;
+            case "copas":
+                return 1;
+            case "espadas":
+                return 2;
+            case "bastos":
+                return 3;
+            default:
+                return -1;
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         int x = 50;
         int y = 50;
+        Color backgroundColor = new Color(250, 239, 193);
 
         // Dibujar las cartas en el lienzo
         for (Card card : cards) {
             if (card.isFaceUp()) {
-                g.setColor(Color.WHITE);
-                g.fillRect(x, y, 100, 150);
-                g.setColor(Color.BLACK);
-                g.drawRect(x, y, 100, 150);
-                g.drawString(card.toString(), x + 10, y + 20);
+                int suitIndex = getSuitIndex(card.getSuit());
+
+                if (suitIndex != -1 && suitIndex < cardImages.length && cardImages[suitIndex] != null) {
+                    g.setColor(backgroundColor);
+                    g.fillRect(x, y, 100, 150);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(x, y, 100, 150);
+                    g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 30));
+                    g.drawString(Integer.toString(card.getRank()), x + 30, y + 85);
+                    g.drawImage(cardImages[suitIndex], x + 5, y + 5, null);
+                } else {
+                    // Si no se encuentra la imagen del palo, dibujar un rectángulo
+                    g.setColor(Color.WHITE);
+                    g.fillRect(x, y, 100, 150);
+                    g.setColor(Color.BLACK);
+                    g.drawRect(x, y, 100, 150);
+                    g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 80));
+                    g.drawString(card.toString(), x + 10, y + 20);
+                }
             } else {
                 g.setColor(Color.BLUE);
                 g.fillRect(x, y, 100, 150);
